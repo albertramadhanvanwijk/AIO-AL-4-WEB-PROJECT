cc0baeb5f009973f06b0420a66d27dd47312be45
albertramadhanvanwijk <albertramadhanvanwijk@gmail.com>
2024-05-07 10:18:43 +0700

    refactor: integration emailNotification fo show in email (error)

--- Back-end/src/services/NotificationService.js
+++ Back-end/src/services/NotificationService.js
@@ -20,7 +20,7 @@ const generateConfig = async (type, param) => {
     if(type == 'requestApproval') {
         title = "Permintaan Persetujuan Part.";
         // target_user = await userModel.getByRoleId(3); 
-        target_user = [{email: 'albertramadhanvanwijk@gmail.com'},{email: 'adnanwafeeq1@gmail.com'}]
+        target_user = [{email: 'albertramadhanvanwijk@gmail.com'}]
         files.email = "bodyEmailNotifRequestApproval.html";
     } else {
         console.log('Notification type not found!')
